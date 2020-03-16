07630e4d91443b0e5056a9a5c34e677ac9dedfb5
George Luiz Moraes Rosa Junior <george.junior@rsinet.com.br>
2020-03-16 17:52:55 -0300

    teste gitignore e screenshots

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,12 +2,12 @@
 #   Capybara.page.driver.browser.manage.window.maximize
 # end
 
-# After do |scenario|
-#   scenario_name = scenario.name.gsub(/\s+/, "_").tr("/", "_")
+After do |scenario|
+  scenario_name = scenario.name.gsub(/\s+/, "_").tr("/", "_")
 
-#   if scenario.failed?
-#     tirar_foto(scenario_name.downcase!, "falhou")
-#   else
-#     tirar_foto(scenario_name.downcase!, "passou")
-#   end
-# end
+  if scenario.failed?
+    tirar_foto(scenario_name.downcase!, "falhou")
+  else
+    tirar_foto(scenario_name.downcase!, "passou")
+  end
+end
