06b4f5e18c28b7461f6514e047b6644e526181c3
George Luiz Moraes Rosa Junior <george.junior@rsinet.com.br>
2020-03-16 15:17:54 -0300

    adequacao para headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,8 +9,19 @@ require_relative "page_helper.rb"
 World(Helper)
 World(Pages)
 
+case ENV["BROWSER"]
+when "firefox"
+  @driver = :selenium
+when "chrome"
+  @driver = :selenium_chrome
+when "headless"
+  @driver = :selenium_chrome_headless
+else
+  puts "Invalid browser"
+end
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = @driver
   config.app_host = "https://www.advantageonlineshopping.com"
   config.default_max_wait_time = 10
 end
